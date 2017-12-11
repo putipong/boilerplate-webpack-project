350c278379d885a9f732c48719b355e5bfc98a5b
Matt Putipong <mattputipong@gmail.com>
2017-12-10 22:34:57 -0500

    prod webpack config

--- config/webpack.prod.config.js
+++ config/webpack.prod.config.js
@@ -3,3 +3,27 @@
  */
 
 'use strict';
+
+import webpack from 'webpack';
+import Merge from 'webpack-merge';
+import UglifyJsPlugin from 'uglifyjs-webpack-plugin';
+import CommonConfig from './webpack.base.config';
+
+export default Merge( CommonConfig, {
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				loader: 'babel-loader'
+			}
+		]
+	},
+	plugins: [
+		new webpack.LoaderOptionsPlugin( {
+			minimize: false,
+			debug: false
+		} ),
+		new webpack.optimize.ModuleConcatenationPlugin(),
+		new UglifyJsPlugin()
+	]
+} );
