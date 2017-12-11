37754cc371af58f3f81c6c066e1e7959366c4000
Matt Putipong <mattputipong@gmail.com>
2017-12-10 22:33:05 -0500

    dev webpack config

--- config/webpack.dev.config.js
+++ config/webpack.dev.config.js
@@ -3,3 +3,17 @@
  */
 
 'use strict';
+
+import { resolve } from 'path';
+import Merge from 'webpack-merge';
+import CommonConfig from './webpack.base.config';
+
+export default Merge( CommonConfig, {
+	context: resolve( __dirname, '../src' ),
+	devServer: {
+		compress: true,
+		port: 8080,
+		publicPath: '/',
+		contentBase: './dist'
+	}
+} );
